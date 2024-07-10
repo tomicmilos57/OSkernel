2f32a359f708086da37af44b4f386a388781a303
tomicmilos57 <tomicmilos57@gmail.com>
2024-07-10 18:59:22 +0200

    Fixed timedWait and sem_close

--- src/riscv.cpp
+++ src/riscv.cpp
@@ -1,208 +1,180 @@
 #include "../h/riscv.hpp"
-#include "../h/tcb.hpp"
+#include "../h/console.hpp"
+#include "../h/memory.hpp"
 #include "../h/print.hpp"
 #include "../h/semaphore.hpp"
-#include "../h/memory.hpp"
-#include "../h/console.hpp"
-#define a0(x)                                 \
-    __asm__ volatile("mv t0, %0" : : "r"(x)); \
-    __asm__ volatile("sw t0, 80(x8)");
-void Riscv::popSppSpie()
-{
-    Riscv::S = false;
-    __asm__ volatile("csrw sepc, ra");
-    mc_sstatus(SSTATUS_SPP);
-    __asm__ volatile("sret");
+#include "../h/tcb.hpp"
+
+#define a0(x)                                                                  \
+  __asm__ volatile("mv t0, %0" : : "r"(x));                                    \
+  __asm__ volatile("sw t0, 80(x8)");
+void Riscv::popSppSpie() {
+  Riscv::S = false;
+  __asm__ volatile("csrw sepc, ra");
+  mc_sstatus(SSTATUS_SPP);
+  __asm__ volatile("sret");
 }
 bool Riscv::S;
-void Riscv::handleSupervisorTrap()
-{
-    S = true;
-    uint64 scause = r_scause();
-    uint64 volatile code, a1, a2, a3, a4;
-    __asm__ volatile("mv %[code], a0" : [code] "=r"(code));
-    __asm__ volatile("mv %[a1], a1" : [a1] "=r"(a1));
-    __asm__ volatile("mv %[a2], a2" : [a2] "=r"(a2));
-    __asm__ volatile("mv %[a3], a3" : [a3] "=r"(a3));
-    __asm__ volatile("mv %[a4], a4" : [a4] "=r"(a4));
-    if (scause == 0x0000000000000009UL || scause == 0x0000000000000008UL)
-    {                                        // ecall
-        uint64 volatile sepc = r_sepc() + 4; // ecall return to its address
-        uint64 volatile sstatus = r_sstatus();
-        switch (code)
-        {
-        case MEM_ALLOC:
-        {  
-            void *ptr = malloc((size_t)a1);
-            a0((uint64)ptr) break;
-        }
-        case MEM_FREE:
-        {
-            int r = free((void *)a1);
-            a0((uint64)r) break;
-        }
-        case THREAD_CREATE:
-        {
-            TCB* volatile val = TCB::createThread((void(*)(void*))a2, (void*)a3);
-            (*(thread_t**)a1) = (thread_t*)val;
-            if(val){a0(0)}
-            else {a0(-1)}
-            break;
-        }
-        case THREAD_EXIT:
-        {   
-            TCB::thread_exit();
-            a0(0)
-            break;
-        }
+void Riscv::handleSupervisorTrap() {
+  S = true;
+  uint64 scause = r_scause();
+  uint64 volatile code, a1, a2, a3, a4;
+  __asm__ volatile("mv %[code], a0" : [code] "=r"(code));
+  __asm__ volatile("mv %[a1], a1" : [a1] "=r"(a1));
+  __asm__ volatile("mv %[a2], a2" : [a2] "=r"(a2));
+  __asm__ volatile("mv %[a3], a3" : [a3] "=r"(a3));
+  __asm__ volatile("mv %[a4], a4" : [a4] "=r"(a4));
+  if (scause == 0x0000000000000009UL ||
+      scause == 0x0000000000000008UL) {  // ecall
+    uint64 volatile sepc = r_sepc() + 4; // ecall return to its address
+    uint64 volatile sstatus = r_sstatus();
+    switch (code) {
+    case MEM_ALLOC: {
+      void *ptr = malloc((size_t)a1);
+      a0((uint64)ptr) break;
+    }
+    case MEM_FREE: {
+      int r = free((void *)a1);
+      a0((uint64)r) break;
+    }
+    case THREAD_CREATE: {
+      TCB *volatile val = TCB::createThread((void (*)(void *))a2, (void *)a3);
+      (*(thread_t **)a1) = (thread_t *)val;
+      if (val) {
+        a0(0)
+      } else {
+        a0(-1)
+      }
+      break;
+    }
+    case THREAD_EXIT: {
+      TCB::thread_exit();
+      a0(0) break;
+    }
 
-        case SEM_OPEN:
-        {
-            Sem **handle = (Sem**)a1;
-            Sem *semaphore = new Sem(a2);
-            if(semaphore == nullptr) a0(-1);
-            
-            *handle = semaphore;
-            Scheduler::putSem(semaphore);
-            a0(0)
-            break;
-        }
-        case SEM_CLOSE:
-        {
-            Sem* handle = (Sem*)a1;
-            if(handle == nullptr || !Scheduler::semaphoreExists(handle)){
-                a0(-1)
-                break;
-            }
-            Scheduler::removeSemaphore(handle);
-            //SprintLine("Num of Semaphores: ", Scheduler::getSemaphoresNumber());
-            a0(0)
-            break;
-        }
-        case SEM_WAIT:
-        {
-            Sem* handle = (Sem*)a1;
-            if(!Scheduler::semaphoreExists(handle)){
-                a0(-1)
-                break;
-            }
-            a0(handle->wait())
-            break;
-        }
-        case SEM_SIGNAL:
-        {
-            Sem* handle = (Sem*)a1;
-            if(!Scheduler::semaphoreExists(handle)){
-                a0(-1)
-                break;
-            }
-            a0(handle->signal())
-            //break; //after signal -> go to dispatch
-        }
-        case THREAD_DISPATCH:
-        {
-            uint64 volatile sepc_yield = r_sepc() + 4; 
-            uint64 volatile sstatus_yield = r_sstatus();
-            TCB::timeSLiceCounter = 0;
-            if (TCB::running->sleeping > 0)
-                TCB::dispatchSleep();
-            else
-                TCB::dispatch();
-            w_sstatus(sstatus_yield);
-            w_sepc(sepc_yield);
-            break;
-        }
-        case SEM_TIMEDWAIT:
-        {
-            Sem* handle = (Sem*)a1;
-            uint64 timeout = a2;
-            if(handle == nullptr){
-                a0(-1)
-                break;
-            }
-            int ret = handle->timedwait(timeout);
-            a0(ret)
-            break;
-        }
-        case SEM_TRYWAIT:
-        {
-            Sem* handle = (Sem*)a1;
-            if(!Scheduler::semaphoreExists(handle)){
-                a0(-1)
-                break;
-            }
-            a0(handle->trywait())
-            break;
-        }
-        case TIME_SLEEP:
-        {
-            a0(TCB::time_sleep(a1));
-            break;
-        }
-        case GETC:
-        {
-            a0((uint64)MyConsole::__getc());
-            break;
-        }
-        case PUTC:{
-            MyConsole::__putc((char)a1);
-            break;
-        }
-        }
-        w_sstatus(sstatus);
-        w_sepc(sepc);
-    }
-    else if (scause == 0x8000000000000001UL)
-    { // interrupt, supervisor software interrupt (timer)
-        TCB::timeSLiceCounter++;
-        for (uint64 i = 0; i < Scheduler::getNumOfSleep(); i++) 
-        {                                                       
-            TCB *elem = Scheduler::getSleep();
-            elem->sleeping--;
-            if (elem->sleeping > 0)
-                Scheduler::putSleep(elem);
-            else{
-                elem->sleeping = 0;
-                Scheduler::put(elem);
-            }
-        }
-        Scheduler::wakeUpSleepingSemaphores();
+    case SEM_OPEN: {
+      Sem **handle = (Sem **)a1;
+      Sem *semaphore = new Sem(a2);
+      if (semaphore == nullptr)
+        a0(-1);
 
-        if (TCB::timeSLiceCounter >= TCB::running->getTimeSlice())
-        {
-            uint64 volatile sepc = r_sepc();
-            uint64 volatile sstatus = r_sstatus();
-            TCB::timeSLiceCounter = 0;
-            TCB::dispatch();
-            w_sstatus(sstatus);
-            w_sepc(sepc);
-        }
-        mc_sip(SIP_SSIP);
+      *handle = semaphore;
+      Scheduler::putSem(semaphore);
+      a0(0) break;
     }
-    else if (scause == 0x8000000000000009UL)
-    { // interrupt, supervisor external interrupt (console)
-        
-        int irq = plic_claim();
-        
-        while (*((char*)(CONSOLE_STATUS)) & CONSOLE_RX_STATUS_BIT) {
-           char c = (*(char*)CONSOLE_RX_DATA);
-           MyConsole::putFromKeyboard(c);
-        }
-        plic_complete(irq);
+    case SEM_CLOSE: {
+      Sem *handle = (Sem *)a1;
+      if (handle == nullptr || !Scheduler::semaphoreExists(handle)) {
+        a0(-1) break;
+      }
+      Scheduler::removeSemaphore(handle);
+      // SprintLine("Num of Semaphores: ", Scheduler::getSemaphoresNumber());
+      a0(0) break;
     }
-    else
-    { // unexpected trap cause
-        
-        SprintString("Unexpected trap cause\n");
-        // print scause
-        SprintLine("Scause: ", r_scause());
-        // print sepc
-        SprintLine("Sepc: ", r_sepc());
-        // print stval
-        SprintLine("Stval: ", r_stval());
-        
-        // while(1);
-        //TCB::time_sleep(1000);
+    case SEM_WAIT: {
+      Sem *handle = (Sem *)a1;
+      if (!Scheduler::semaphoreExists(handle)) {
+        a0(-1) break;
+      }
+      a0(handle->wait()) break;
     }
-    S = false;
+    case SEM_SIGNAL: {
+      Sem *handle = (Sem *)a1;
+      if (!Scheduler::semaphoreExists(handle)) {
+        a0(-1) break;
+      }
+      a0(handle->signal())
+      // break; //after signal -> go to dispatch
+    }
+    case THREAD_DISPATCH: {
+      uint64 volatile sepc_yield = r_sepc() + 4;
+      uint64 volatile sstatus_yield = r_sstatus();
+      TCB::timeSLiceCounter = 0;
+      if (TCB::running->sleeping > 0)
+        TCB::dispatchSleep();
+      else
+        TCB::dispatch();
+      w_sstatus(sstatus_yield);
+      w_sepc(sepc_yield);
+      break;
+    }
+    case SEM_TIMEDWAIT: {
+      Sem *handle = (Sem *)a1;
+      uint64 timeout = a2;
+      if (!Scheduler::semaphoreExists(handle)) {
+        a0(-1) break;
+      }
+      int ret = handle->timedwait(timeout);
+      a0(ret) break;
+    }
+    case SEM_TRYWAIT: {
+      Sem *handle = (Sem *)a1;
+      if (!Scheduler::semaphoreExists(handle)) {
+        a0(-1) break;
+      }
+      a0(handle->trywait()) break;
+    }
+    case TIME_SLEEP: {
+      a0(TCB::time_sleep(a1));
+      break;
+    }
+    case GETC: {
+      a0((uint64)MyConsole::__getc());
+      break;
+    }
+    case PUTC: {
+      MyConsole::__putc((char)a1);
+      break;
+    }
+    }
+    w_sstatus(sstatus);
+    w_sepc(sepc);
+  } else if (scause == 0x8000000000000001UL) { // interrupt, supervisor software
+                                               // interrupt (timer)
+    TCB::timeSLiceCounter++;
+    for (uint64 i = 0; i < Scheduler::getNumOfSleep(); i++) {
+      TCB *elem = Scheduler::getSleep();
+      elem->sleeping--;
+      if (elem->sleeping > 0)
+        Scheduler::putSleep(elem);
+      else {
+        elem->sleeping = 0;
+        Scheduler::put(elem);
+      }
+    }
+    Scheduler::wakeUpSleepingSemaphores();
+
+    if (TCB::timeSLiceCounter >= TCB::running->getTimeSlice()) {
+      uint64 volatile sepc = r_sepc();
+      uint64 volatile sstatus = r_sstatus();
+      TCB::timeSLiceCounter = 0;
+      TCB::dispatch();
+      w_sstatus(sstatus);
+      w_sepc(sepc);
+    }
+    mc_sip(SIP_SSIP);
+  } else if (scause == 0x8000000000000009UL) { // interrupt, supervisor external
+                                               // interrupt (console)
+
+    int irq = plic_claim();
+
+    while (*((char *)(CONSOLE_STATUS)) & CONSOLE_RX_STATUS_BIT) {
+      char c = (*(char *)CONSOLE_RX_DATA);
+      MyConsole::putFromKeyboard(c);
+    }
+    plic_complete(irq);
+  } else { // unexpected trap cause
+
+    SprintString("Unexpected trap cause\n");
+    // print scause
+    SprintLine("Scause: ", r_scause());
+    // print sepc
+    SprintLine("Sepc: ", r_sepc());
+    // print stval
+    SprintLine("Stval: ", r_stval());
+
+    // while(1);
+    // TCB::time_sleep(1000);
+  }
+  S = false;
 }
--- src/semaphore.cpp
+++ src/semaphore.cpp
@@ -1,111 +1,119 @@
 #include "../h/semaphore.hpp"
-#include "../h/tcb.hpp"
 #include "../h/scheduler.hpp"
+#include "../h/tcb.hpp"
 
-void Sem::putBlock(TCB *tcb){blockedQueue.addLast(tcb);}
-TCB *Sem::getBlock(){return blockedQueue.removeFirst();}
 
-int Sem::block(){
-    TCB *old = TCB::running;
+void Sem::putBlock(TCB *tcb) { blockedQueue.addLast(tcb); }
+TCB *Sem::getBlock() { return blockedQueue.removeFirst(); }
 
-    putBlock(old);
-    //Scheduler::putSleep(old);
+int Sem::block() {
+  TCB *old = TCB::running;
 
-    TCB::running = Scheduler::get();
+  putBlock(old);
+  // Scheduler::putSleep(old);
 
-    TCB::contextSwitch(&old->context, &TCB::running->context);
-    return 0;
+  TCB::running = Scheduler::get();
+
+  TCB::contextSwitch(&old->context, &TCB::running->context);
+  return 0;
 }
-void Sem::unblock(){
-    TCB* blocked = getBlock();
-    if(blocked != 0){
-    blocked->sleeping = 0; 
-    //Scheduler::put(blocked);
+void Sem::unblock() {
+  TCB *blocked = getBlock();
+  if (blocked != 0) {
+    if (blocked->sleeping != 0)
+      val++;
+    blocked->sleeping = 0;
+    blocked->timeout = 0;
+    // Scheduler::put(blocked);
     Scheduler::putFirst(blocked);
-    }
+  }
 }
-int Sem::wait(){
-    TCB *old = TCB::running;
+int Sem::wait() {
+  TCB *old = TCB::running;
 
-    if(--val < 0) putBlock(old);
-    else Scheduler::put(old);
-    TCB::running = Scheduler::get();
+  if (--val < 0)
+    putBlock(old);
+  else
+    Scheduler::put(old);
+  TCB::running = Scheduler::get();
 
-    TCB::contextSwitch(&old->context, &TCB::running->context);
+  TCB::contextSwitch(&old->context, &TCB::running->context);
 
-    TCB *temp = TCB::getRunning();
-    if(temp->timeout == 1) {
-        temp->timeout = 0;
-        return -1; 
-    }
+  TCB *temp = TCB::getRunning();
+  if (temp->timeout == 1) {
+    temp->timeout = 0;
+    return -1;
+  }
 
+  return 0;
+}
+int Sem::trywait() {
+  if (--val < 0)
+    return block();
+  return 1;
+}
+
+int Sem::timedwait(time_t timeout) {
+  if (timeout <= 0)
     return 0;
+  TCB *old = TCB::running;
+
+  if (--val < 0) {
+    old->sleeping = timeout;
+    putBlock(old);
+  } else
+    Scheduler::put(old);
+  TCB::running = Scheduler::get();
+
+  TCB::contextSwitch(&old->context, &TCB::running->context);
+  TCB *temp = TCB::getRunning();
+  if (temp->timeout == 2) {
+    temp->timeout = 0;
+    return -2;
+  } else if (temp->timeout == 1) {
+    temp->timeout = 0;
+    return -1;
+  }
+  return 0;
 }
-int Sem::trywait(){
-    if(--val < 0) return block();
-    return 1;
+int Sem::signal() {
+
+  if (++val <= 0)
+    unblock();
+
+  return 0;
+}
+int Sem::close() {
+  while (blockedQueue.getN() > 0) {
+    TCB *thread = getBlock();
+    Scheduler::put(thread);
+  }
+  return 0;
 }
 
-int Sem::timedwait(time_t timeout){
-    if(timeout <= 0) return 0;
-    TCB *old = TCB::running;
-    
-    if(--val < 0) {
-        old->sleeping = timeout;
-        putBlock(old);
-    }
-    else Scheduler::put(old);
-    TCB::running = Scheduler::get();
+void Sem::wakeUp() {
 
-    TCB::contextSwitch(&old->context, &TCB::running->context);
-    TCB *temp = TCB::getRunning();
-    if(temp->timeout == 2) {
-        temp->timeout = 0;
-        return -2; 
-    }else if(temp->timeout == 1) {
-        temp->timeout = 0;
-        return -1; 
-    }
-    return 0;
+  for (uint64 i = 0; i < blockedQueue.getN(); i++) {
+    TCB *elem = blockedQueue.removeFirst();
+    if (elem->sleeping == 1) {
+      elem->sleeping = 0;
+      elem->timeout = 2;
+      Scheduler::put(elem);
+    } else if (elem->sleeping > 1) {
+      elem->sleeping--;
+      blockedQueue.addLast(elem);
+    } else
+      blockedQueue.addLast(elem);
+  }
 }
-int Sem::signal(){
-    
-    if(++val <= 0) unblock();
-    
-    return 0;
-}
-int Sem::close(){
-    while(blockedQueue.getN() > 0){
-        TCB* thread = getBlock();
-        Scheduler::put(thread);
+void Sem::unblockAll() {
+  for (uint64 i = 0; i < blockedQueue.getN(); i++) {
+    TCB *blocked = getBlock();
+    i--;
+    if (blocked != 0) {
+      blocked->sleeping = 0;
+      blocked->timeout = 1;
+      Scheduler::putFirst(blocked);
     }
-    return 0;
-}
-
-
-void Sem::wakeUp(){
-
-    for (uint64 i = 0; i < blockedQueue.getN(); i++) 
-    {                                                       
-        TCB *elem = blockedQueue.removeFirst();
-        if (elem->sleeping == 1){
-            elem->sleeping = 0;
-            elem->timeout = 2;
-            Scheduler::put(elem);
-        }
-        else if(elem->sleeping > 1) {
-            elem->sleeping--;
-            blockedQueue.addLast(elem);
-        }else blockedQueue.addLast(elem);
-    }
-}
-void Sem::unblockAll(){
-    for (uint64 i = 0; i < blockedQueue.getN(); i++) {
-        TCB* blocked = getBlock();
-        if(blocked != 0){
-            blocked->sleeping = 0;
-            blocked->timeout = 1;
-            Scheduler::putFirst(blocked);
-        }
-    }
+  }
 }
